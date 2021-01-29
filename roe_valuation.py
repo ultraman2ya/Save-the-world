12d1282713b7b4b5218124ec499799d548a3e7b6
ultraman2ya <am770920@gmail.com>
2021-01-29 16:59:13 +0900

    next step

--- roe_valuation.py
+++ roe_valuation.py
@@ -45,9 +45,18 @@ def get_download_kosdaq():
     df.종목코드 = df.종목코드.map('{:06d}.KQ'.format)
     return df
 
+def get_spread_point():
+    url ="https://www.kisrating.com/ratingsStatistics/statics_spread.do"
+    sp = urllib.request.urlopen(url).read()
+    sp_soup=BeautifulSoup(sp, 'html.parser')
+    sp_cells = sp_soup.find('div', {'class': "table_ty1"}).find_all('td')
+    return sp_cells[98].string
+    
+
 # kospi, kosdaq 종목코드 각각 다운로드
 kospi_df = get_download_kospi()
 kosdaq_df = get_download_kosdaq()
+spread_point = get_spread_point()
 
 # data frame merge
 code_df = pd.concat([kospi_df, kosdaq_df])
@@ -58,18 +67,65 @@ code_df = code_df[['회사명', '종목코드']]
 # data frame title 변경 '회사명' = name, 종목코드 = 'code'
 code_df = code_df.rename(columns={'회사명': 'name', '종목코드': 'code'})
 
-code_df = code_df.head(2)
+code_df = code_df.head(1)
+
+stock_frame = pd.DataFrame(columns = ['code','name','roe','stockholder','all_stock','my_stock'])
 
 for k_code, k_name in zip(code_df['code'],code_df['name']):
     try:
+        #ROE줍줍
         url ="http://comp.fnguide.com/SVO2/ASP/SVD_FinanceRatio.asp?pGB=1&gicode=A" + k_code[:-3] + "&cID=&MenuYn=Y&ReportGB=&NewMenuID=104&stkGb=701"
-
+        #print(url)
         f = urllib.request.urlopen(url).read()
         soup=BeautifulSoup(f, 'html.parser')
-        cells = soup.find('tr', {'id': "p_grid1_18"}).find_all('td')
-        for cell in cells:
-            print (cell.string)
+        roe_cells = soup.find('tr', {'id': "p_grid1_18"}).find_all('td')
+        print(roe_cells[4].string)
+
+        #지배주주 줍줍
+        #http://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode=A155660&cID=&MenuYn=Y&ReportGB=&NewMenuID=103&stkGb=701
+        url ="http://comp.fnguide.com/SVO2/ASP/SVD_Finance.asp?pGB=1&gicode=A" + k_code[:-3] + "&cID=&MenuYn=Y&ReportGB=&NewMenuID=103&stkGb=701"
+        #print(url)
+        f = urllib.request.urlopen(url).read()
+        soup=BeautifulSoup(f, 'html.parser')
+        sh_cells = soup.find('tr', {'id': "p_grid2_10"}).find_all('td')
         
+        print((sh_cells[3].string).replace(',',''))
+
+        ##발행주식수 줍줍
+        #http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A010130&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701
+        url ="http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A" + k_code[:-3] + "&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701"
+        print(url)
+        f = urllib.request.urlopen(url).read()
+        soup=BeautifulSoup(f, 'html.parser')
+
+        cells = soup.find('table', {'class': "us_table_ty1 table-hb thbg_g h_fix zigbg_no"}).find_all('td')
+        all_stock_cnt = ((cells[10].string).replace(',','')).split('/')[0]
+
+        cells = soup.find('table', {'class': "us_table_ty1 h_fix zigbg_no notres"}).find_all('td')
+        
+        print(cells.len())
+        
+        '''
+        if cells.len > 6:
+            my_stock_cnt = ((cells[6].string).replace(',','')).split('/')[0]
+        else:
+            my_stock_cnt=0
+        
+
+        stock_frame = stock_frame.append({'code':k_code,
+                                          'name':k_name,
+                                          'roe':roe_cells[4].string,
+                                          'stockholder':(sh_cells[3].string).replace(',',''),
+                                          'all_stock':all_stock_cnt,
+                                          'my_stock':my_stock_cnt}, 
+                                          ignore_index = True)
+        #stock_cnt = (all_stock_cnt - my_stock_cnt) / 100000000
+
+        #지배주주지분 + ((지배주주지분 * (예상roe - bbb_roe)/bbb_roe)
+        #stock_price = c_stockholder + ((c_stockholder * (roe - spread_point))/spread_point)
+
+        print(stock_frame)
+        '''
     except:
         pass
 
