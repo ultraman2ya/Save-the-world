d8c5c7dee80f5fa41a9023fa3901ff0993fee8c9
ultraman2ya <am770920@gmail.com>
2021-02-02 16:03:54 +0900

    2

--- test_holder.py
+++ test_holder.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import urllib.request
 
-url ="http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A010130&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701"
-#url = "http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A353200&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701"
+#url ="http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A010130&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701"
+url = "http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A353200&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701"
 
 print(url)
 f = urllib.request.urlopen(url).read()
@@ -12,18 +12,27 @@ cells = soup.find_all('table')
 
 aaaa = cells[10].find_all('tr')
 row_cnt = 0
-roe_position = 0
+title_position1 = 0
+title_position2 = 0
 
 for row in aaaa:
     row_cnt = row_cnt +1
     bbb = row.find('div')
     
     if bbb:
-        len_c = len(bbb.string)
-        if ((bbb.string) == '  지배주주순이':
-            roe_position = row_cnt
+        print(bbb.string)
+        if (bbb.string) == '자본총계':
+            title_position1 = row_cnt
             print(row_cnt)
+        
+        if (bbb.string) == '자본금':
+            title_position2 = row_cnt
+            print(row_cnt)
+                
 
-xxx = aaaa[roe_position - 1 ].find_all('td')
+if (title_position2 - title_position1)>1:
+    xxx = aaaa[title_position1].find_all('td')
+else:
+    xxx = aaaa[title_position1 - 1 ].find_all('td')
 
 print(xxx)
