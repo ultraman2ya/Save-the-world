e6be4767b74d77c9ab85e0ba3e1277ffec3cdf8e
ultraman2ya <am770920@gmail.com>
2021-02-02 15:44:32 +0900

    etc test files

---
+++ test_holder.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+
+url ="http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A010130&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701"
+#url = "http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&gicode=A353200&cID=&MenuYn=Y&ReportGB=&NewMenuID=101&stkGb=701"
+
+print(url)
+f = urllib.request.urlopen(url).read()
+soup=BeautifulSoup(f, 'html.parser')
+cells = soup.find_all('table')
+
+aaaa = cells[10].find_all('tr')
+row_cnt = 0
+roe_position = 0
+
+for row in aaaa:
+    row_cnt = row_cnt +1
+    bbb = row.find('div')
+    
+    if bbb:
+        len_c = len(bbb.string)
+        if ((bbb.string) == '  지배주주순이':
+            roe_position = row_cnt
+            print(row_cnt)
+
+xxx = aaaa[roe_position - 1 ].find_all('td')
+
+print(xxx)
